8f00b4eae9255d5fb4da2c656f287f65a58991ad
“Serena” <serena8744@gmail.com>
2018-11-30 19:56:42 -0600

    input file version

--- src/main/java/team/cs425/g54/CraneMaster.java
+++ src/main/java/team/cs425/g54/CraneMaster.java
@@ -109,6 +109,7 @@ public class CraneMaster {
                 jsonMsg.put("children", arr);
                 InetAddress address = InetAddress.getByName(record.getIpAddr());
                 logger.info("sending task to Node" + record.getID() + " Addr "+record.getIpAddr());
+                logger.info("send msg: "+jsonMsg.toString());
                 DatagramPacket send_message = new DatagramPacket(jsonMsg.toString().getBytes(), jsonMsg.toString().getBytes().length, address,Detector.sendTaskPort);
                 server.send(send_message);
             }
