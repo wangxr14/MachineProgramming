1ddceb8be6dbbf969fbcb05eb4ab72f93d43b54f
xinranw5 <xinranw5@illinois.edu>
2018-12-01 01:24:19 -0600

    ignore empty line

--- src/main/java/team/cs425/g54/SpoutThread.java
+++ src/main/java/team/cs425/g54/SpoutThread.java
@@ -49,6 +49,10 @@ public class SpoutThread extends Thread {
 				while(line!=null) {
 					System.out.println("Line is:"+line);
 					linenumber++;
+					if(line.isEmpty()) {
+						line = bufferedReader.readLine();
+						continue;
+					}
 					HashMap<String,String> emit=new HashMap<String, String>();
 					emit.put(Integer.toString(linenumber), line);
 					sendTuple(emit);
