1bc39fa296aef9207c3baf25db71bf704e9ab15f
“Serena” <serena8744@gmail.com>
2018-12-02 19:16:51 -0600

    fix bug

--- spark_python/spark_join.py
+++ spark_python/spark_join.py
@@ -8,13 +8,18 @@ from pyspark.streaming import StreamingContext
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.exit(-1)
-        
+
     sc = SparkContext(appName="filterApp")
     ssc = StreamingContext(sc, 10)
     #text = sc,sys.argv[2]  # local file
+    file = sys.argv[2]
+    local = open(file,'r')
+    local_lines = []
+    for line in local.readlines():
+        local.append(line)
     lines1 = ssc.textFileStream(sys.argv[1]) #streaming file dir1
-    lines2 = ssc.textFileStream(sys.argv[2]) #streaming file dir2
-    result = lines1.join(lines2)
+    #lines2 = ssc.textFileStream(sys.argv[2]) #streaming file dir2
+    result = lines1.flatMap(lambda line: [(line + l) for l in local_lines])
     result.pprint()
     result.saveAsTextFiles("join_result")
     ssc.start()
